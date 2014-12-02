8fe59e76634d3e33399639faf75381df8767197a
Peter Pham <phamanpeter@gmail.com>
2014-12-02 15:28:47 -0800

    FOUR COLUMNS!

--- src/omari/SuperPowers/AvatarState.java
+++ src/omari/SuperPowers/AvatarState.java
@@ -10,23 +10,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AvatarState extends JavaPlugin {
-	private static int height = 5;
+	private static double height = 5;
 	private static double initialHeight;
+	private static double initialWidth;
+	public static double width = 5;
 
 	public static void catAttack(CommandSender sender) {
 		sender.sendMessage("WORK YOU PIECE OF AVATAR!");
 	}
 	
-	public static void rainMaker(Player p) {
+	public static void glassMaker(Player p) {
 
 //		Location loc = p.getPlayer().getLocation();
 //		loc.setY(loc.getY() + height);
 //		Block b = loc.getBlock();
 //		b.setType(Material.WATER);
-		
 		Location loc = p.getLocation();
 		initialHeight = loc.getY();
-		
 		
 		loc.setX(loc.getX() + 1);
 		makeVerticleBlocks(loc);
@@ -46,6 +46,29 @@ public class AvatarState extends JavaPlugin {
 		makeVerticleBlocks(loc);
 	}
 	
+	public static void fourColumns(Player p) {
+		Location loc = p.getLocation();
+		double james = loc.getX();
+		double morgan = loc.getZ();
+		double ahmad = loc.getY();
+		
+		loc.setY(ahmad + 5); //initial height
+		loc.setX(james + 5);
+		Block bl = loc.getBlock();
+		bl.setType(Material.WATER);
+		loc.setX(james-5); 
+		bl = loc.getBlock();
+		bl.setType(Material.WATER);
+		loc.setX(james); //revert x to default
+		
+		loc.setZ(morgan + 5);
+		bl = loc.getBlock();
+		bl.setType(Material.WATER);
+		loc.setZ(morgan-5); 
+		bl = loc.getBlock();
+		bl.setType(Material.WATER);
+	}
+	
 	public static void makeVerticleBlocks(Location loc) {
 		loc.setY(initialHeight);
 		for (int i = 0; i < height; i++) {
@@ -53,6 +76,15 @@ public class AvatarState extends JavaPlugin {
 			
 			Block bl = loc.getBlock();
 			bl.setType(Material.GLASS);
+		}
+	}
+	
+	public static void makeHorizantolBlocks(Location loc) {
+		loc.setX(initialHeight);
+		for (int i = 0; i < width; i++) {
+			loc.setX(loc.getX() + 1);
+			Block bl = loc.getBlock();
+			bl.setType((Material.DIRT));
 		}
 	}
 	@Override
